fe19001436cf239ce2291067c0e730e14f8d5268
Brandon Lau <brandonlau8888@gmail.com>
2024-12-02 00:54:50 -0500

    241202 - google cloud run deployed

--- app/src/main/java/com/budgetapp/budgetapp/util/Constant.kt
+++ app/src/main/java/com/budgetapp/budgetapp/util/Constant.kt
@@ -4,6 +4,7 @@ import com.budgetapp.budgetapp.BuildConfig
 
 object Constant {
 //    const val BASE_URL = "http://192.168.1.23:8080"
-    const val BASE_URL = "http://173.52.45.250:8080"
+//    const val BASE_URL = "http://173.52.45.250:8080"
+    const val BASE_URL = "https://budgetting-api-56751984313.us-east4.run.app"
     const val CLIENT_ID = BuildConfig.CLIENT_ID
 }
